52a1169819956fa7da71ba66f34b703eaa29d1df
Molly Carroll <molly.carroll@kroger.com>
2022-05-13 14:41:34 -0600

    random experimental changes to check colors

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
+    title: "Serapis",
     description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+      "Serapis is a local St. Louis rock band.",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
